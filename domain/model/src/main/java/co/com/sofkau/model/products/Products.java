736aba00ea5dbb78c0c4fc097c444c9061f52fa6
Cristian Quiñones <camilo.santamaria322@gmail.com>
2021-09-07 16:58:35 -0500

    Se prueba primer endpoint agregar con reactividad

--- domain/model/src/main/java/co/com/sofkau/model/products/Products.java
+++ domain/model/src/main/java/co/com/sofkau/model/products/Products.java
@@ -2,8 +2,8 @@ package co.com.sofkau.model.products;
 import lombok.*;
 
 @Data
-//@NoArgsConstructor
-//@AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder(toBuilder = true)
 public class Products {
     private String id;
@@ -12,54 +12,5 @@ public class Products {
     private int precio;
     private int lote;
 
-    public Products() {
-    }
 
-    public Products(String id, String codigo, String nombre, int precio, int lote) {
-        this.id = id;
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.lote = lote;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(int precio) {
-        this.precio = precio;
-    }
-
-    public int getLote() {
-        return lote;
-    }
-
-    public void setLote(int lote) {
-        this.lote = lote;
-    }
 }
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/sofkau/api/dtos/ProductsDTO.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/sofkau/api/dtos/ProductsDTO.java
@@ -5,8 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-//@NoArgsConstructor
-//@AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductsDTO {
     private String id;
     private String codigo;
@@ -14,54 +14,5 @@ public class ProductsDTO {
     private int precio;
     private int lote;
 
-    public ProductsDTO() {
-    }
 
-    public ProductsDTO(String id, String codigo, String nombre, int precio, int lote) {
-        this.id = id;
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.lote = lote;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(int precio) {
-        this.precio = precio;
-    }
-
-    public int getLote() {
-        return lote;
-    }
-
-    public void setLote(int lote) {
-        this.lote = lote;
-    }
 }
