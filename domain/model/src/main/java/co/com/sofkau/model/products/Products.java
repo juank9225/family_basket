d03adbaaa1504dc150375caf8d964c55a7c21502
Juan Salcedo <juank9225@gmail.com>
2021-09-07 14:13:24 -0500

    Se crea la estructura general del proyecto base con arquitectura limpia

---
+++ domain/model/src/main/java/co/com/sofkau/model/products/Products.java
@@ -0,0 +1,8 @@
+package co.com.sofkau.model.products;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder(toBuilder = true)
+public class Products {
+}
---
+++ domain/model/src/main/java/co/com/sofkau/model/products/gateways/ProductsRepository.java
@@ -0,0 +1,4 @@
+package co.com.sofkau.model.products.gateways;
+
+public interface ProductsRepository {
+}
