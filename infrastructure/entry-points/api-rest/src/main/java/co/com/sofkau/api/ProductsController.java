57aafd08fab0180b48ecf3b22cb8026dd4acfe38
Juan Salcedo <juank9225@gmail.com>
2021-09-10 10:42:52 -0500

    Se repara error al momento de correr al aplicacion

--- infrastructure/entry-points/api-rest/src/main/java/co/com/sofkau/api/ProductsController.java
+++ infrastructure/entry-points/api-rest/src/main/java/co/com/sofkau/api/ProductsController.java
@@ -3,29 +3,30 @@ import co.com.sofkau.api.dtos.ProductsDTO;
 import co.com.sofkau.usecase.createproducts.CreateProductsUseCase;
 import co.com.sofkau.usecase.createproducts.ListProductsUseCase;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping(value = "/api/product",produces = MediaType.APPLICATION_JSON_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api",produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
 public class ProductsController {
 
     private final CreateProductsUseCase createProductsUseCase;
     private final ListProductsUseCase listProductsUseCase;
 
-    @PostMapping(path = "/crear")
+    @PostMapping(path = "/product/crear")
     public Mono<ProductsDTO> guardarProducts(@RequestBody ProductsDTO productsDTO){
         return createProductsUseCase.execute(ProductsDTO.convertirDTOADominioCreacion(productsDTO))
                 .map(ProductsDTO::convertidorDominioADTO);
     }
 
-    @GetMapping(path = "/list")
-    public Flux<ProductsDTO> listarProductos(){
-        return listProductsUseCase.execute().map(ProductsDTO::convertidorDominioADTO);
+    @GetMapping(path = "/product/list")
+    public ResponseEntity<Flux<ProductsDTO>> listarProductos(){
+        return new  ResponseEntity(listProductsUseCase.execute().map(ProductsDTO::convertidorDominioADTO), HttpStatus.OK);
     }
 
 }
